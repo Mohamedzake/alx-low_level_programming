d4b9536104e324db7100fdd5b7cb92253877670a
mohamed <zakei7671@gmail.com>
2023-06-15 20:20:15 -0700

    g

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
-#include <unistd.h> 
+#include <unistd.h>
 
-/** 
-* main - Entry point 
-* 
-* Description: A C programme prints some string 
-* 
-* Return: Always 1 (Error) 
-*/ 
+/**
+ * main - Entry point
+ *
+ * Description: A C programme prints some string
+ *
+ * Return: Always 1 (Error)
+*/
 
-int main(void) 
-{ 
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
-        return (1); 
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
