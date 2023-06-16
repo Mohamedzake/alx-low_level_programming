1e504ea2e37bd8218446a431533c29181f74e567
mohamed <zakei7671@gmail.com>
2023-06-16 06:54:05 -0700

    d

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print  aplhabet ( lower ,upper)
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char ch = 'a';
+	char ch = 'A';
+
+	while (ch <= 'z')
+	{
+	putchar(ch);
+	ch++;
+	}
+
+	while (CH <= 'z')
+	{
+	putchar(CH);
+	CH++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
