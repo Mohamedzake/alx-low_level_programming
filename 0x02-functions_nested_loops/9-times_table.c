b8a0e808e8782922233eea0273c8d6a1c362f5c7
mohamed <zakei7671@gmail.com>
2023-06-20 19:09:20 -0700

    h

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,20 +4,33 @@
 /**
  * times_table - prints the 9 times table, starting with 0.
  *
- * @n && result: take the input from another
+ * @(i, j) && result: take the input from another
  *
  * Return:the 9 times table.
 */
 
 void times_table(void)
 {
-	int n;
-	int result;
+	int i, j, result;
 
-	for (n = 0; n <= 10; n++)
+	for (i = 0; i <= 9; i++)
 	{
-	result = 9 * n;
-	_putchar((result + '0'));
+	for (j = 0; j <= 9; j++)
+	{
+	result = i * j;
+	if (result < 10)
+	{
+	_putchar(result + '0');
+
+	}
+	else
+	{
+	_putchar((result / 10) + '0');
+	_putchar((result % 10) + '0');
+	}
+	_putchar('\t');
+	}
 	_putchar('\n');
 	}
+
 }
