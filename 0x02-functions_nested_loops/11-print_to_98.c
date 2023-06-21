d4ff429d72ae72190d2a3d6aede7b21f8391e1f7
mohamed <zakei7671@gmail.com>
2023-06-21 08:05:44 -0700

    k

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
 * print_to_98 - checks for alphabetic character
 *
 * @n: take the input from another
 *
 */
+
 void print_to_98(int n)
 {
 	int i;
 
-        if (n < 98)
+	if (n < 98)
 	{
 	for (i = n; i <= 98; i++)
 	{
