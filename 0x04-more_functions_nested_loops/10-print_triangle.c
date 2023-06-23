2263a50d30d91ff500d5f3d74b1c310dee574281
mohamed <zakei7671@gmail.com>
2023-06-22 17:33:51 -0700

    h

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line.
+ *
+ * @size: take the input frpm another
+ *
+*/
+
+void print_triangle(int size)
+{
+
+	int i, j, n;
+	int num = size;
+
+	if (size > 0)
+	{
+	for (i = 0; i < size; i++)
+	{
+	for (j = 1; j < num; j++)
+	{
+	_putchar(' ');
+	}
+	for (n = 0; n <= i; n++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	num--;
+	}
+
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -29,7 +29,7 @@ int main(void)
 	{
 	printf("Buzz");
 	{
-	else
+	else 
 	{
 	printf("%i", i);
 	}
