ec78f1ac49f92b324735783356a3a943780890bd
mohamed <zakei7671@gmail.com>
2023-06-26 03:41:50 -0700

    l

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,6 +4,7 @@
 
 /**
  * rev_string - reverses a string.
+ *
  *@s: take the input from another
  * Return: void
  */
@@ -15,13 +16,23 @@ void rev_string(char *s)
 
 	length = strlen(s);
 
-	for (i = 0, j = length - 1; i < j; i++, j--) {
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
 	char temp = s[i];
+
 	s[i] = s[j];
 	s[j] = temp;
 	}
 }
 
+/**
+ * print_rev - reverses a string. - prints a string,
+ *	in reverse, followed by a new line.
+ *
+ *
+ *@s: take the input from another
+ * Return: void
+ */
 void print_rev(char *s)
 {
 	s = "Hello, World!";
