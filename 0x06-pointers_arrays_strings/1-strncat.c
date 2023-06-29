3b051b6f60b6b2eede528e5fe0d116b670dfcf9e
mohamed <zakei7671@gmail.com>
2023-06-29 00:40:54 -0700

    k

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,10 +8,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	char *dest_pt = dest;
 
+	while (*dest_pt != '\0') {
+	dest_pt++;
+	}
 
+	while (n > 0 && *src != '\0') {
+	*dest_pt = *src;
+	dest_pt++;
+	src++;
+	n--;
+	}
+	*dest_pt = '\0';
 
-
-
-
+	return dest;
 }
