93e9eb7fb2fd46cedd0173b192e870f481a72918
mohamed <zakei7671@gmail.com>
2023-06-29 18:27:04 -0700

    j

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,15 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  **leet - encodes a string into 1337.
- *
- * Return: Always void.
- */
-char *leet(char *)
+ *@str: take the input from another
+ *Return: str
+*/
+char *leet(char *str)
 {
+	int i, j;
+	char leet_char[5] = {'4', '3', '0', '7', '1'};
+	char normal_char[5][3] = {{"aA"}, {"eE"}, {"oO"}, {"tT"}, {"lL"}};
 
-
-
-
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	for (j = 0; j < 5; j++)
+	{
+	if (str[i] == normal_char[j][0] || str[i] == normal_char[j][1])
+	{
+	str[i] = leet_char[j];
+	break;
+	}
+	}
+	}
+	return (str);
 }
