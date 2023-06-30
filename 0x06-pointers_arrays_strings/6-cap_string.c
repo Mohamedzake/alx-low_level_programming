bd431bf24fa2c1dba98ac4e89fa8c3f59fdc785c
mohamed <zakei7671@gmail.com>
2023-06-29 18:13:43 -0700

    j

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,11 +3,31 @@
 
 /**
  **cap_string - capitalizes all words of a string.
- *
+ *@str: take the input from another
  * Return: Always void.
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
+	int i;
 
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+	str[0] -= 32;
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	if (str[i - 1] == ' '  str[i - 1] == '\t'  str[i - 1] == '\n'
+	str[i - 1] == ','  str[i - 1] == ';'  str[i - 1] == '.'
+	str[i - 1] == '!'  str[i - 1] == '?'  str[i - 1] == '"'
+	str[i - 1] == '('  str[i - 1] == ')'  str[i - 1] == '{'
+	str[i - 1] == '}'
+	{
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+	str[i] -= 32;
+	}
+	}
+	}
+	return (str);
 
 }
