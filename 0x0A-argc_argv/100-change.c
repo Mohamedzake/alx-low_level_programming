cbf6748cd48d60fc0313c0ef64f5ec8802c85382
mohamed <zakei7671@gmail.com>
2023-07-07 05:57:08 -0700

    k

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entery point
+ * Description: prints the minimum number of coins to make change
+ * for an amount of money.
+ * @argc: takes input from another.
+ * @argv: takes input from another
+ * Return: always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	int cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+	printf("0\n");
+	return (0);
+	}
+
+	int coins = 0;
+
+	coins += cents / 25;
+	cents %= 25;
+
+	coins += cents / 10;
+	cents %= 10;
+
+	coins += cents / 5;
+	cents %= 5;
+	coins += cents / 2;
+	cents %= 2;
+
+	coins += cents;
+
+	printf("%d\n", coins);
+
+	return (0);
+}
