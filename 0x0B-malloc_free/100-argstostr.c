2125548a9f30096423179500470e887f67876a3d
mohamed <zakei7671@gmail.com>
2023-07-11 10:54:31 -0700

    j

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,31 +2,27 @@
 #include <string.h>
 
 char *argstostr(int ac, char **av)
-{
+{ int total_lengt = 0;
+    int i, j;
+    char *str;
+int position = 0;
     if (ac == 0 || av == NULL)
         return NULL;
 
-    int total_length = 0;
-    int i, j;
-    char *str;
 
-    /* Calculate the total length of the concatenated string */
+ 
+
     for (i = 0; i < ac; i++)
     {
-        total_length += strlen(av[i]);
+        total_lengt += strlen(av[i]);
     }
 
-    /* Add space for newlines */
-    total_length += ac - 1;
+    total_lengt += ac - 1;
 
-    /* Allocate memory for the concatenated string */
-    str = (char *)malloc(sizeof(char) * (total_length + 1));
+    str = (char *)malloc(sizeof(char) * (total_lengt + 1));
     if (str == NULL)
         return NULL;
 
-    int position = 0;
-
-    /* Copy each argument and add newline */
     for (i = 0; i < ac; i++)
     {
         for (j = 0; av[i][j] != '\0'; j++)
@@ -42,7 +38,6 @@ char *argstostr(int ac, char **av)
         }
     }
 
-    /* Null-terminate the concatenated string */
     str[position] = '\0';
 
     return str;
