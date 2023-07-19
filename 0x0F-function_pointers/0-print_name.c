e270038ca9bee325333f85c1561dda5409bf7d0b
mohamed <zakei7671@gmail.com>
2023-07-19 16:38:53 -0700

    j

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include "function_pointers.h"
 /**
  * print_name - prints a name
  * @name: name of the person
- *@f:take the input from another.
- * Return: Nothing.
+ *@f:take the input
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL && f != NULL)
 	f(name);
 }
 
