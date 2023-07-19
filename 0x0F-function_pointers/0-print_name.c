1341641926d370cb75a543ae36ee2bd30b1ddee4
mohamed <zakei7671@gmail.com>
2023-07-19 05:16:41 -0700

    j

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 /**
  * print_name - prints a name
  * @name: name of the person
- *@name:take the input from another.
+ *@f:take the input from another.
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
