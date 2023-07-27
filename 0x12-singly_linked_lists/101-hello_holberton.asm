bd2128cad289a7c35b40d5c5da27657e44c906fe
mohamed <zakei7671@gmail.com>
2023-07-27 16:26:21 -0700

    j

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,22 @@
 ; 101-hello_holberton.asm
 
 section .data
-	hello db "Hello, Holberton", 0
+    hello db "Hello, Holberton", 0
 
 section .text
-	global _start
+    global _start
 
 _start:
-	mov     rax, 1                  ; syscall number for sys_write
-	mov     rdi, 1                  ; file descriptor for stdout
-	mov     rsi, hello              ; pointer to the string to be printed
-	mov     rdx, 16                 ; length of the string
-	syscall                         ; invoke the syscall
-	mov     rax, 60                 ; syscall number for sys_exit
-	xor     rdi, rdi                ; exit status 0
-	syscall                         ; invoke the syscall
+    ; Call main function from the C standard library
+    call main
+
+    ; Exit the program
+    mov     rax, 60                 ; syscall number for sys_exit
+    xor     rdi, rdi                ; exit status 0
+    syscall                         ; invoke the syscall
+
+; Define the main function (empty for now)
+section .bss
+section .text
+    extern main
 
---
+++ 0x12-singly_linked_lists/101-main.c
@@ -0,0 +1,10 @@
+/* 101-main.c */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("This is the main function.\n");
+    return 0;
+}
+
