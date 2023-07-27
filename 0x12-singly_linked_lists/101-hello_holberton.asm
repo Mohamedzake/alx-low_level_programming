47b4e199a810fc45b080f9148c999fc6ba323344
mohamed <zakei7671@gmail.com>
2023-07-27 16:12:50 -0700

    j

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,18 @@
 ; 101-hello_holberton.asm
 
 section .data
-    hello db "Hello, Holberton", 0
+	hello db "Hello, Holberton", 0
 
 section .text
-    global _start
+	global _start
 
 _start:
-    mov     rax, 1                  ; syscall number for sys_write
-    mov     rdi, 1                  ; file descriptor for stdout
-    mov     rsi, hello              ; pointer to the string to be printed
-    mov     rdx, 16                 ; length of the string
-    syscall                         ; invoke the syscall
-    mov     rax, 60                 ; syscall number for sys_exit
-    xor     rdi, rdi                ; exit status 0
-    syscall                         ; invoke the syscall
+	mov     rax, 1                  ; syscall number for sys_write
+	mov     rdi, 1                  ; file descriptor for stdout
+	mov     rsi, hello              ; pointer to the string to be printed
+	mov     rdx, 16                 ; length of the string
+	syscall                         ; invoke the syscall
+	mov     rax, 60                 ; syscall number for sys_exit
+	xor     rdi, rdi                ; exit status 0
+	syscall                         ; invoke the syscall
 
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,37 +14,37 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
-    
+
 	if (str == NULL)
-        return NULL;
+	return (NULL);
 
-    
-    if (new_node == NULL)
-        return NULL;
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return NULL;
-    }
+	if (new_node == NULL)
+	return (NULL);
 
-    new_node->len = strlen(str);
-    new_node->next = NULL;
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+	free(new_node);
+	return (NULL);
+	}
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-    }
-    else
-    {
-        list_t *last_node = *head;
-        while (last_node->next != NULL)
-            last_node = last_node->next;
+	new_node->len = strlen(str);
+	new_node->next = NULL;
 
-        last_node->next = new_node;
-    }
+	if (*head == NULL)
+	{
+	*head = new_node;
+	}
+	else
+	{
+	list_t *last_node = *head;
 
-    return new_node;
+	while (last_node->next != NULL)
+	last_node = last_node->next;
+
+	last_node->next = new_node;
+	}
+
+	return (new_node);
 }
-
