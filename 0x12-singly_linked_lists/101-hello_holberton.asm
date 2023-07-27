d9ee3eaeeeaeada98b6b3196e31a7236edab7f26
mohamed <zakei7671@gmail.com>
2023-07-27 16:34:19 -0700

    j

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,19 @@
-; 101-hello_holberton.asm
-
 section .data
-    hello db "Hello, Holberton", 0
+    hello db "Hello, Holberton", 10, 0    ; 10 is the ASCII code for newline (\n)
 
 section .text
-    global _start
+    extern printf
 
-_start:
-    ; Call main function from the C standard library
-    call main
+global main
+
+main:
+    push rbp
+    mov rdi, hello
+    call printf
+    pop rbp
 
     ; Exit the program
-    mov     rax, 60                 ; syscall number for sys_exit
-    xor     rdi, rdi                ; exit status 0
-    syscall                         ; invoke the syscall
-
-; Define the main function (empty for now)
-section .bss
-section .text
-    extern main
+    mov rax, 60                 ; syscall number for sys_exit
+    xor rdi, rdi                ; exit status 0
+    syscall                     ; invoke the syscall
 
