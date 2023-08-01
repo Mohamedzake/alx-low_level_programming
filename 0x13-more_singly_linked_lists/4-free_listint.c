843ae80cdcaee637fc4611202cbea35114b83d7c
mohamed <zakei7671@gmail.com>
2023-08-01 06:22:54 -0700

    j

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,15 +1,18 @@
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ *free_listint - frees a listint_t list.
+ *@head: take the input from another>
+ * Return: Always 0.
+ */
 void free_listint(listint_t *head)
 {
-    listint_t *current;
+	listint_t *current;
 
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+	while (head != NULL)
+	{
+	current = head;
+	head = head->next;
+	free(current);
+	}
 }
-
