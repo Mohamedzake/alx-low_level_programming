267c784ffbe60392cc5c3ba7badf0a70f106cb34
mohamed <zakei7671@gmail.com>
2023-08-07 16:58:30 -0700

    j

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,8 +7,8 @@
 /**
  * read_textfile - Reads a text file and
  *	prints it to the POSIX standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters it should read and print.
+ * @filename:take the input from another.
+ * @letters:take the input from another.
  *
  * Return: The actual number of letters it could read and print.
  */
