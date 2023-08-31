2af34a317ce5f26b904901c2369c6acb75db2ec6
mohamed <zakei7671@gmail.com>
2023-08-31 10:26:36 -0700

    j

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,11 +1,6 @@
-#include "main.h"
-#include <stdio.h>
-/**
- * _putchar - Writes a character to the standard output (stdout)
- * @c: take the input from user.
- * Return: value.
- */
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
+#include <unistd.h> 
+ 
+int _putchar(char c) 
+{ 
+    return write(1, &c, 1); 
 }
