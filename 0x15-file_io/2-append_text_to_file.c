0794876695f17296100b3b6fb6058a58f3449bd8
mohamed <zakei7671@gmail.com>
2023-09-04 09:46:10 -0700

    j

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,25 +3,35 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
-int append_text_to_file(const char *filename, char *text_content) {
-   int fd;
-       	if (filename == NULL)
-        return (-1);
+/**
+ * append_text_to_file - main function.
+ *@filename: take the input from another.
+ *@text_content: take the input from another.
+ * Return: value.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
 
-     fd = open(filename, O_WRONLY | O_APPEND);
+	if (filename == NULL)
+	return (-1);
 
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-    if (text_content != NULL) {
-        ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1) {
-            close(fd);
-            return (-1);
-        }
-    }
+	if (fd == -1)
+	return (-1);
 
-    close(fd);
-    return (1);
+	if (text_content != NULL)
+	{
+	ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+
+	if (bytes_written == -1)
+	{
+	close(fd);
+	return (-1);
+	}
+	}
+
+	close(fd);
+	return (1);
 }
-
