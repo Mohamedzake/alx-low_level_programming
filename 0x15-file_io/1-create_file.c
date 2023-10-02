d951fe6d1ba1826160a0dce81bab48ae3ec30038
mohamed <zakem7671@gmail.com>
2023-10-02 05:06:47 -0700

    j

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 	return (-1);
 
-	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, file_permissions);
+	file_descriptor =
+	open(filename, O_WRONLY | O_CREAT | O_TRUNC, file_permissions);
 	if (file_descriptor == -1)
 	return (-1);
 
